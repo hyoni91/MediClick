fb51836ddc426943dedeb895d9ddaa5098687d05
hyoni91 <hyoni.green@gmail.com>
2024-08-21 10:13:30 +0900

    first

---
+++ src/main/java/com/green/MediClick/MediClickApplication.java
@@ -0,0 +1,13 @@
+package com.green.MediClick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediClickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediClickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/green/MediClick/MediClickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.MediClick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediClickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
