b63a5c221ee58404eb48b941592e249f1cd1c3c3
hyoni91 <hyoni.green@gmail.com>
2024-08-21 12:43:44 +0900

    memberVO

--- src/main/java/com/green/MediClick/member/controller/MemberController.java
+++ src/main/java/com/green/MediClick/member/controller/MemberController.java
@@ -1,4 +1,12 @@
 package com.green.MediClick.member.controller;
 
+import lombok.Data;
+
+@Data
 public class MemberController {
+    private int memNum;
+    private String memName;
+    private String memTel;
+    private String memRrn;
+    private String memRole;
 }
