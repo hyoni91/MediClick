6280a330eb7453c868cfda77b2765216816e4935
ajw29k <alswjdgma12@gmail.com>
2024-08-22 15:23:59 +0900

    회원가입 code 작성 ì

--- src/main/java/com/green/MediClick/member/controller/MemberController.java
+++ src/main/java/com/green/MediClick/member/controller/MemberController.java
@@ -2,7 +2,10 @@ package com.green.MediClick.member.controller;
 
 
 import com.green.MediClick.member.service.MemberService;
+import com.green.MediClick.member.vo.MemberVO;
 import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +15,9 @@ public class MemberController {
 
     @Resource(name = "memberService")
     private MemberService memberService;
+
+    @PostMapping("/insertMember")
+    public void insertMember(@RequestBody MemberVO memberVO){
+        memberService.insertMember(memberVO);
+    }
 }
