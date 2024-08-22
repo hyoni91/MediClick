83854c1f4a1319a8d1a807bd5596dc1c0770d6c7
hotmnil5 <hotmnil5@gmail.com>
2024-08-22 17:41:56 +0900

    react 배너 수정

---
+++ src/main/frontend/src/user/UserLayout.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const UserLayout = () => {
+  return (
+    <div>
+      <div className='menu-bar'>
+          {/* 메뉴 바 */}
+          <ul>
+            <li><span>병원안내</span></li>
+            <li><span>진료과/의료진</span></li>
+            <li><span>진료예약</span></li>
+            <li><span>고객서비스</span></li>
+          </ul>
+        </div>
+    </div>
+  )
+}
+
+export default UserLayout
