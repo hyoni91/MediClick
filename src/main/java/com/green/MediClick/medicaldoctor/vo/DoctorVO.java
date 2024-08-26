257664fb07877f90f31be0fb70edbc1bd804b65a
ajw29k <alswjdgma12@gmail.com>
2024-08-26 17:52:19 +0900

    kfdjskdfjskdfjsdf

--- src/main/java/com/green/MediClick/medicaldoctor/vo/DoctorVO.java
+++ src/main/java/com/green/MediClick/medicaldoctor/vo/DoctorVO.java
@@ -2,15 +2,11 @@ package com.green.MediClick.medicaldoctor.vo;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class DoctorVO {
     private int docNum;
     private String docName;
-<<<<<<< HEAD
-    private MedicalDept medicalDept;
-=======
-    private int deptNum;
-    private MedicalDept deptVO;
->>>>>>> e7d9067c758fb05f4d91551e3b1230668f97b4b0
-
+    private List<MedicalDept> medicalDept;
 }
