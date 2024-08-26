37cf723a24b89e878e6274cb513280796633c00d
penguinnhi <tork1355@gmail.com>
2024-08-26 15:42:57 +0900

    memListjava

--- src/main/java/com/green/MediClick/schedule/service/ScheduleService.java
+++ src/main/java/com/green/MediClick/schedule/service/ScheduleService.java
@@ -1,5 +1,11 @@
 package com.green.MediClick.schedule.service;
 
+import com.green.MediClick.schedule.vo.ScheduleVO;
+
+import java.util.List;
+
 public interface ScheduleService {
+
+    List<ScheduleVO> getDocMem(int docNum);
 
 }
