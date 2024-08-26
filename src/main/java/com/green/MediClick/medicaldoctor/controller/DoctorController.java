a63a22a278bef0303690807fa3d914d82a966970
hotmnil5 <hotmnil5@gmail.com>
2024-08-26 16:16:55 +0900

    의사 진료진 정보 조회 ë©임시저장

--- src/main/java/com/green/MediClick/medicaldoctor/controller/DoctorController.java
+++ src/main/java/com/green/MediClick/medicaldoctor/controller/DoctorController.java
@@ -1,13 +1,23 @@
 package com.green.MediClick.medicaldoctor.controller;
 
 import com.green.MediClick.medicaldoctor.service.DoctorService;
+import com.green.MediClick.medicaldoctor.vo.DoctorVO;
 import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/")
 public class DoctorController {
     @Resource(name = "doctorService")
     private DoctorService doctorService;
+
+    // 의사 정보 조회
+    @GetMapping("/doctorList")
+    public List<DoctorVO> getDoctorList(){
+        return doctorService.getDoctorList();
+    }
 }
