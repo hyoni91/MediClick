aaa3dc31a7ded11bd059c071f3b3d73531f1b551
hyoni91 <hyoni.green@gmail.com>
2024-08-27 09:21:30 +0900

    충돌

--- src/main/java/com/green/MediClick/medicaldoctor/vo/DoctorVO.java
+++ src/main/java/com/green/MediClick/medicaldoctor/vo/DoctorVO.java
@@ -2,14 +2,14 @@ package com.green.MediClick.medicaldoctor.vo;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class DoctorVO {
     private int docNum;
     private String docName;
-
     private MedicalDept medicalDept;
-
     private int deptNum;
     private MedicalDept deptVO;
-
+    private List<MedicalDept> medicalDept;
 }
