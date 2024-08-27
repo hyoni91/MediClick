6101881669043140825e4f380787d0e80b565823
ajw29k <alswjdgma12@gmail.com>
2024-08-27 15:05:59 +0900

    회원가입 완료

--- src/main/frontend/src/user/JoinForm.js
+++ src/main/frontend/src/user/JoinForm.js
@@ -1,11 +1,15 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from 'react'
 import './JoinForm.css'
 import axios from 'axios'
 import { useNavigate, useNavigation } from 'react-router-dom'
 const JoinForm = () => {
   //회원가입 담을 변수
-  const [memberData, setMemberData] = useState({memRole : 'User'})
+  const [memberData, setMemberData] = useState({
+    memName: '',
+    memRrn: '',
+    memTel: '',
+    memRole : 'USER'
+  })
 
   //이동페이지 할수있게 네비게이션
   const navigate = useNavigate()
@@ -17,17 +21,19 @@ const JoinForm = () => {
       , [e.target.name] : e.target.value
     })
   }
+  const regex = /^\d{2}(0[1-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0-1])[-]\d{7}$/
   //전화번호 정규식
-  const [phoneNumber, setPhoneNumber] = useState('');
+  const [memTel, setMemTel] = useState('');
   const autoHyphen2 = (e) => {
     const value = e.target.value
       .replace(/[^0-9]/g, '')
       .replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, "$1-$2-$3")
       .replace(/(\-{1,2})$/g, "");
     
-    setPhoneNumber(value);
+      setMemTel(value);
   };
   const insertJoin = () => {
+    console.log(memberData)
     axios.post('/member/insertMember',memberData)
     .then((res) => {
       navigate('/')
@@ -38,18 +44,27 @@ const JoinForm = () => {
     <div>
         <div><h1>회원가입</h1></div>
       <div className='join-div'>
-        <div>
-          <div>회원이름</div>
-          <div><input name='memName' type='text' placeholder="이름을 입력하세요" onChange={(e) => {changeData(e)}}/></div>
-          <div>회원주민번호</div>
-          <div><input name='memRrn' type='password' placeholder="주민번호를 입력하세요" onChange={(e) => {changeData(e)}}/></div>
-          <div>전화번호</div>
-          <div>
-          
-    <input name='memTel' type="text" onChange={(e)=>{autoHyphen2(e)}} value={phoneNumber} maxlength="13" placeholder="전화번호를 입력하세요" />
-          </div>
+        <table className='join-table'>
+          <tbody>
+            <tr>
+              <td>회원이름</td>
+              <td><input name='memName' type='text' placeholder="이름을 입력하세요" onChange={(e) => {changeData(e)}}/></td>
+            </tr>
+            <tr>
+              <td>회원주민번호</td>
+              <td><input name='memRrn' type='password' placeholder="주민번호를 입력하세요" onChange={(e) => {changeData(e)}}/></td>
+            </tr>
+            <tr>
+              <td>전화번호</td>
+              <td>
+              
+        <input name='memTel' type="text" onChange={(e)=>{
+          autoHyphen2(e)
+          }} value={memTel} maxLength='13' placeholder="전화번호를 입력하세요" /></td>
+            </tr>
+          </tbody>
+        </table>
           <div><button className='join-btn' onClick={() => {insertJoin()}}>가입하기</button></div>
-        </div>
         
       </div>
         <div className='relative-container'>
