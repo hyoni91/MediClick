b5e6231a243c321ebf42a41195b4231bf839eca2
hotmnil5 <hotmnil5@gmail.com>
2024-09-02 09:19:36 +0900

    0902 시작

--- src/main/frontend/src/user/UserLayout.js
+++ src/main/frontend/src/user/UserLayout.js
@@ -7,23 +7,6 @@ const UserLayout = () => {
 
   return (
     <div >
-
-<<<<<<< HEAD
-      <div className='int'>
-        <div className='layout-bar'>
-          <div className='logoimg-div' onClick={(e)=>{navigate('/')}}>
-            <img className='logo' src='http://localhost:8080/images/66135a3db29c4ef5faa06efb.png'/></div>
-          <div className='menu-bar'>
-              {/* 메뉴 바 */}
-              <ul>
-                <li><span>병원안내</span></li>
-                <li><span onClick={()=>{navigate('/medicalDoctor')}}>진료과/의료진</span></li>
-                <li><span onClick={()=>{navigate('/scheduleForm')}}>진료예약</span></li>
-                <li><span>고객서비스</span></li>
-              </ul>
-            </div>
-       </div>
-=======
       <div className='layout-bar'>
         <div className='logoimg-div' onClick={(e)=>{navigate('/')}}>
           <img className='logo' src='http://localhost:8080/images/66135a3db29c4ef5faa06efb.png'/></div>
@@ -36,10 +19,7 @@ const UserLayout = () => {
               <li><span onClick={()=>{navigate('/userService')}}>고객서비스</span></li>
             </ul>
           </div>
->>>>>>> SES
       </div>
-
-
         <Outlet/>
     </div>
   )
--- src/main/java/com/green/MediClick/medicaldoctor/controller/DoctorController.java
+++ src/main/java/com/green/MediClick/medicaldoctor/controller/DoctorController.java
@@ -16,39 +16,37 @@ public class DoctorController {
 
     // 의사 정보 조회
     @GetMapping("/doctorList")
-    public List<DoctorVO> getDoctorList(){
+    public List<DoctorVO> getDoctorList() {
         return doctorService.getDoctorList();
     }
 
     @GetMapping("/oneDoctor/{docNum}")
-    public DoctorVO getOneDoctor(@PathVariable("docNum")String docNum){
+    public DoctorVO getOneDoctor(@PathVariable("docNum") String docNum) {
         return doctorService.getOneDoctor(docNum);
     }
 
     // 다음에 들어갈 이미지 조회한후
     // 상세정보 눌러서 이미지 수정하기
     @PostMapping("/insertDoctorImg/{docNum}")
-    public void insertDoctorImg(@RequestBody DoctorVO doctorVO){
+    public void insertDoctorImg(@RequestBody DoctorVO doctorVO) {
         doctorVO.setDocNum(doctorService.nextDoctorImg());
     }
 
     // 회원가입 후 의사 정보 조회
     @GetMapping("/selectDoctor/{docNum}")
-    public DoctorVO selectDoctor(@PathVariable("docNum") String docNum){
+    public DoctorVO selectDoctor(@PathVariable("docNum") String docNum) {
         return doctorService.selectDoctor(docNum);
     }
 
     // 회원가입 후 관리자면 의사정보 추가
     @PostMapping("/insertDoctor")
-    public void insertDoctor(@RequestBody DoctorVO doctorVO){
+    public void insertDoctor(@RequestBody DoctorVO doctorVO) {
         doctorService.insertDoctor(doctorVO);
     }
 
     // 관리자 입력상태에서 취소시 삭제
-    @GetMapping("/deleteDoctor/{docNum")
-    public void deleteDoctor(@PathVariable ("docNum") String docNum){
+    @GetMapping("/deleteDoctor/{docNum}")
+    public void deleteDoctor(@PathVariable("docNum") String docNum) {
         doctorService.deleteDoctor(docNum);
     }
-
-
 }
