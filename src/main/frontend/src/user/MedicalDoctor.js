7d9d8db49d3812a1486a8f2abb5402f418cc8055
hotmnil5 <hotmnil5@gmail.com>
2024-09-02 10:57:23 +0900

    list 수정

--- src/main/frontend/src/user/MedicalDoctor.js
+++ src/main/frontend/src/user/MedicalDoctor.js
@@ -37,7 +37,7 @@ const MedicalDoctor = () => {
                 {docotrList[0].docName != '' ? docotrList[0].docName : <></>}
               </div>
               <div className='introDoctorDept'>
-                {docotrList[0].medicalDept[0].deptName}
+                {docotrList[0].medicalDept.deptName}
               </div>
               <div className='introDoctor'>
                 유방암의 진단 및 수술적 치료, 유방암의 새로운 치료법 개발을 위한 유전자 연구
@@ -55,7 +55,7 @@ const MedicalDoctor = () => {
                 {docotrList[1].docName != '' ? docotrList[1].docName : <></>}
               </div>
               <div className='introDoctorDept'>
-                {docotrList[1].medicalDept[0].deptName}
+                {docotrList[1].medicalDept.deptName}
               </div>
               <div className='introDoctor'>
                 뇌종양, 뇌전이암 및 연수막암전이 발생기전에 대한 기초연구, <br/>신치료법 개발과 임상시험
@@ -75,7 +75,7 @@ const MedicalDoctor = () => {
                 {docotrList[2].docName != '' ? docotrList[2].docName : <></>}
               </div>
               <div className='introDoctorDept'>
-                {docotrList[2].medicalDept[0].deptName}
+                {docotrList[2].medicalDept.deptName}
               </div>
               <div className='introDoctor'>갑상선암의 방사성 요오드 치료, 진행성 갑상선암의 수술적 치료</div>
               <p className='goSchedule'>
@@ -91,7 +91,7 @@ const MedicalDoctor = () => {
                 {docotrList[3].docName != '' ? docotrList[3].docName : <></>}
               </div>
               <div className='introDoctorDept'>
-                {docotrList[3].medicalDept[0].deptName}
+                {docotrList[3].medicalDept.deptName}
               </div>
               <div className='introDoctor'>자궁내막암, 난소암, 자궁경부암 등의 로봇 및 내시경 수술, 재발성 부인암의 표적항암치료</div>
               <p className='goSchedule'>
@@ -109,7 +109,7 @@ const MedicalDoctor = () => {
                 {docotrList[4].docName != '' ? docotrList[4].docName : <></>}
               </div>
               <div className='introDoctorDept'>
-                {docotrList[4].medicalDept[0].deptName}
+                {docotrList[4].medicalDept.deptName}
               </div>
               <div className='introDoctor'>폐암 및 식도암의 수술적 치료, 폐암의 새로운 치료법 개발을 위한 임상시험 및 약물 유전체 연구</div>
               <p className='goSchedule'>
@@ -125,7 +125,7 @@ const MedicalDoctor = () => {
                 {docotrList[5].docName != '' ? docotrList[5].docName : <></>}
               </div>
               <div className='introDoctorDept'>
-                {docotrList[5].medicalDept[0].deptName}
+                {docotrList[5].medicalDept.deptName}
               </div>
               <div className='introDoctor'>악성림프종, 백혈병, 빈혈 및 혈소판 감소증, 조혈모세포이식, <br/>혈전지혈질환 치료와 연구</div>
               <p className='goSchedule'>
