6621090ed645641e93d7008633e1aacb50d70ec5
ajw29k <alswjdgma12@gmail.com>
2024-09-10 09:17:46 +0900

    sivjmskd

--- src/main/frontend/src/user/test.js
+++ src/main/frontend/src/user/test.js
@@ -11,6 +11,7 @@ import { Line,
   ResponsiveContainer,
   BarChart,
   ScatterChart,
+  Rectangle,
 } from "recharts";
 import './test.css';
 import axios from 'axios';
@@ -38,7 +39,50 @@ const ExampleComponent = () => {
   const formatXAxis = (e) => {
     return `${e}분`
   }
-
+  const data3 = [
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: "Page E",
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: "Page F",
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: "Page G",
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
   // 막대 핸들러
   const [handleBar , setHandleBar] = useState(null)
 console.log(data1)
@@ -50,20 +94,33 @@ console.log(data1)
     <div className='center' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <div>
         <ResponsiveContainer>
-          <BarChart
-          width={600}
-          height={300}
-          data={data1}
-          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="curTem" fill="#8884d8" name="Current Temperature" />
-          <Bar dataKey="avgTem" fill="#82ca9d" name="Average Temperature" />
-        </BarChart>
+        <BarChart
+        width={500}
+        height={300}
+        data={data3}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar
+          dataKey="uv"
+          fill="#B3CDAD"
+          shape={<Rectangle fill="pink" stroke="blue" />}
+        />
+        <Bar
+          dataKey="pv"
+          fill="#FF5F5E"
+          shape={<Rectangle fill="gold" stroke="purple" />}
+        />
+      </BarChart>
         </ResponsiveContainer>
       </div>
       <div>
