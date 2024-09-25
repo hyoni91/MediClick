b9281c8362be3e4f296f8adb7897b2055f6710c6
hyoni91 <hyoni.green@gmail.com>
2024-09-25 16:20:43 +0900

    거래처 체크박스 80프로완성 / ì다중삭제

--- src/main/frontend/src/provider/ManageCustomer.js
+++ src/main/frontend/src/provider/ManageCustomer.js
@@ -13,15 +13,16 @@ const ManageCustomer = () => {
   setModalOpen(!modalOpen)
   }
 
+  //삭제버튼
+  const [deleteCustomer, setDeleteCustomer] = useState(true)
+
   //체크박스 설정
   const [chks, setChks] = useState([])
   const [chkAll, setChkAll] = useState(false)
-  console.log(chks)
 
   //체크 값 받기 
-  const [chkCustomer, setChkCustomer] = useState([])
-
-  console.log(chkCustomer)
+  const [customerNum, setCustomerNum] = useState([])
+  console.log(customerNum)
 
   //거래처 목록
   const [customers, setCustomers] = useState([])
@@ -55,6 +56,23 @@ const ManageCustomer = () => {
     open({onComplete : handleComplete});
   }
 
+
+  //삭제버튼
+  function removeCustomer(){
+    axios.delete(`/customer/deleteCustomer`,{
+      data: customerNum, // 삭제할 고객 ID 목록
+  })
+    .then((res)=>{
+  
+    })
+    .catch((error)=>{
+      alert('error!')
+      console.error('Error Data:', error.response.data);
+      console.error('Error Status:', error.response.status);
+    })
+
+  }
+
   
   //거래처 목록 
   useEffect(()=>{
@@ -73,7 +91,7 @@ const ManageCustomer = () => {
 
   //검색
   function seachCustomer(){
-    axios.post(`/customer/customerList`, searchValue)
+    axios.post(`/customer/customerList`, )
     .then((res)=>{
       setCustomers(res.data)
     })
@@ -121,11 +139,8 @@ const ManageCustomer = () => {
     }
   }
 
-
     //체크박스 함수 
-    const handleCheckAll = (index,e) => {
-      // 체크박스
-      // chkAll이 true일 때 newChks는 모든 체크박스가 false인 배열이 되고, chkAll이 false일 때는 모든 체크박스가 true인 배열이 됨
+    const handleCheckAll = () => {
       const newChks = chks.map(() => !chkAll);
       setChks(newChks);
       setChkAll(!chkAll);
@@ -137,22 +152,25 @@ const ManageCustomer = () => {
       // 특정 인덱스의 상태 변환 후 chks상태 업데이트
       newChks[index] = !newChks[index];
       setChks(newChks);
-      // 모든 체크박스가 선택되었는지 확인
-      //예) newChks.every(chk => chk)는 배열의 모든 요소가 true일 때만 true를 반환
+      //newChks.every(chk => chk)는 배열의 모든 요소가 true일 때만 true를 반환
       setChkAll(newChks.every(chk => chk)); 
+
+        // 하나라도 체크되어 있는지 확인
+      const hasChecked = newChks.some(chk => chk);
+      setDeleteCustomer(!hasChecked); // 상태 업데이트
 
       // 체크된 거래처 상태 업데이트
       if (newChks[index]) {
       // 체크된 경우
-        setChkCustomer(prev => [
+        setCustomerNum(prev => [
         ...prev,
-        {[e.target.name] : e.target.value}  // 객체 형태로 추가
+         e.target.value  // 객체 형태로 추가
         ]);
           } else {
             // 체크 해제된 경우
-                setChkCustomer(prev => 
+              setCustomerNum(prev => 
                   prev.filter(
-                    item => item[e.target.name] !== e.target.value
+                    num => num !== e.target.value
                   ));
         }
     };
@@ -197,7 +215,8 @@ const ManageCustomer = () => {
             </button>
             <button 
               type='button' 
-              disabled={true}
+              disabled={deleteCustomer}
+              onClick={()=>{removeCustomer()}}
             >삭제 <i className="fa-regular fa-trash-can" />
             </button>
           </div>
@@ -231,7 +250,7 @@ const ManageCustomer = () => {
                         type='checkbox'
                         name='customerNum'
                         checked={chks[i]} 
-                        onChange={(e) => handleCheck(i,e) }
+                        onChange={(e) => handleCheck(i,e)}
                       />
                     </td>
                     <td>{customer.customerName}</td>
