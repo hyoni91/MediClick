11f8589e933e5b77832c5ce7a8031db279d0ec15
hyoni91 <hyoni.green@gmail.com>
2024-10-04 16:01:06 +0900

    ì중간저장

--- src/main/java/com/green/MediClick/provider/customers/vo/OrdersVO.java
+++ src/main/java/com/green/MediClick/provider/customers/vo/OrdersVO.java
@@ -12,6 +12,7 @@ public class OrdersVO {
     private String  orderDate;
     private String orderStatus;
     private int totalPrice; // 주문 총액
-    private List<CustomersVO> customersVO;
+    private String customerName;
+    private List<Integer> requesNumtList;
 
 }
