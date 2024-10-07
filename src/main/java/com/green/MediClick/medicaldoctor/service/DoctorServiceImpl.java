aecbb6abe83d56b2df7d6617d4b6bdad45b837f2
ajw29k <alswjdgma12@gmail.com>
2024-10-07 14:28:37 +0900

    kdjfidjfd

--- src/main/java/com/green/MediClick/medicaldoctor/service/DoctorServiceImpl.java
+++ src/main/java/com/green/MediClick/medicaldoctor/service/DoctorServiceImpl.java
@@ -17,18 +17,18 @@ public class DoctorServiceImpl implements DoctorService{
     // 의사 정보 조회
     @Override
     public List<DoctorVO> getDoctorList() {
-        return sqlSession.selectList("mappers.generalMapper.doctorMapper.medicalDoctorList");
+        return sqlSession.selectList("doctorMapper.medicalDoctorList");
     }
 
     @Override
     public DoctorVO getOneDoctor(String docNum) {
-        return sqlSession.selectOne("mappers.generalMapper.doctorMapper.oneDoctor",docNum);
+        return sqlSession.selectOne("doctorMapper.oneDoctor",docNum);
     }
 
     //다음에 들어갈 의사 이미지 조회
     @Override
     public int nextDoctorImg(int docImg) {
-        return sqlSession.selectOne("mappers.generalMapper.doctorMapper.nextDoctorImg",docImg);
+        return sqlSession.selectOne("doctorMapper.nextDoctorImg",docImg);
     }
 
 
@@ -36,38 +36,38 @@ public class DoctorServiceImpl implements DoctorService{
     // 회원가입할때 의사 정보 조회
     @Override
     public DoctorVO selectDoctor(String docNum) {
-        return sqlSession.selectOne("mappers.generalMapper.doctorMapper.selectDoctor",docNum);
+        return sqlSession.selectOne("doctorMapper.selectDoctor",docNum);
     }
 
     @Override
     public void deleteDoctor(String docNum) {
         // 의사 정보 삭제
-        sqlSession.delete("mappers.generalMapper.doctorMapper.deleteDoctor",docNum);
+        sqlSession.delete("doctorMapper.deleteDoctor",docNum);
         // 멤버 정보 삭제
-        sqlSession.delete("mappers.generalMapper.doctorMapper.deleteMember",docNum);
+        sqlSession.delete("doctorMapper.deleteMember",docNum);
     }
 
     //진료과 선택후 저장
     @Override
     public void insertDept(MedicalDept medicalDept) {
-        sqlSession.insert("mappers.generalMapper.doctorMapper.insertDept",medicalDept);
+        sqlSession.insert("doctorMapper.insertDept",medicalDept);
     }
 
     //이미지 저장
     @Override
     public void insertDocImg(DoctorImgVO imgVO) {
-        sqlSession.insert("mappers.generalMapper.doctorMapper.insertDocImg",imgVO);
+        sqlSession.insert("doctorMapper.insertDocImg",imgVO);
     }
 
     @Override
     public void updateDoctor(DoctorVO doctorVO) {
-        sqlSession.update("mappers.generalMapper.doctorMapper.updateDoctor",doctorVO);
+        sqlSession.update("doctorMapper.updateDoctor",doctorVO);
     }
 
     // 회원가입할때 "ADMIN"이면 의사정보도 추가
     @Override
     public void insertDoctor(DoctorVO doctorVO) {
-        sqlSession.insert("mappers.generalMapper.doctorMapper.insertDoctor",doctorVO);
+        sqlSession.insert("doctorMapper.insertDoctor",doctorVO);
     }
 
 }
