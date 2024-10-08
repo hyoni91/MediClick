56c903ce5650c82ed2e2b30cc09a459c09a590d0
ajw29k <alswjdgma12@gmail.com>
2024-10-08 17:24:14 +0900

    delivery

--- src/main/java/com/green/MediClick/provider/delivery/controller/DeliveryController.java
+++ src/main/java/com/green/MediClick/provider/delivery/controller/DeliveryController.java
@@ -1,13 +1,27 @@
 package com.green.MediClick.provider.delivery.controller;
 
+import com.green.MediClick.provider.customers.vo.OrdersVO;
 import com.green.MediClick.provider.delivery.service.DeliveryService;
+import com.green.MediClick.provider.delivery.vo.DeliveryVO;
 import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/delivery")
 public class DeliveryController {
     @Resource(name = "deliveryService")
     DeliveryService deliveryService;
+
+    @GetMapping("/deliveryList")
+    public List<DeliveryVO> deliveryList(){
+        return deliveryService.deliveryList();
+    }
+    @GetMapping("/ordersList")
+    public List<OrdersVO> ordersList(){
+        return deliveryService.ordersList();
+    }
 }
