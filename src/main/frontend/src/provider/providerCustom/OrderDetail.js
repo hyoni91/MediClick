a933ed1e92c2e1c1973a17c145515c54d8911855
hyoni91 <hyoni.green@gmail.com>
2024-10-14 17:10:56 +0900

    재고 파악

--- src/main/frontend/src/provider/providerCustom/OrderDetail.js
+++ src/main/frontend/src/provider/providerCustom/OrderDetail.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import './OrderDetail.css'
 import CheckStock from './CheckStock'
+import { color } from 'chart.js/helpers'
 
 const OrderDetail = () => {
   const navigate = useNavigate();
