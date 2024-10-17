cb058dc01648fa1fc17ad71c135aab10f07573fa
ajw29k <alswjdgma12@gmail.com>
2024-10-17 16:37:12 +0900

    delivery update

--- src/main/frontend/src/provider/DeliryveryCheck.js
+++ src/main/frontend/src/provider/DeliryveryCheck.js
@@ -6,9 +6,9 @@ const DeliryveryCheck = ({setLoginInfo}) => {
   
   const [driver, setDriver] =useState({
     deliveryNum : ''
-    ,orders :''
-    ,deliveryDriverName : loginData?loginData.memName : <></> 
-    ,deliveryDriverPhone :loginData?loginData.memTel : <></>
+    ,orderNum :''
+    ,deliveryDriverName : loginData?loginData.memName : ''
+    ,deliveryDriverPhone : ''
     ,deliveryAddress :''
     ,startTime :''
     ,endTime :''
@@ -26,33 +26,51 @@ const DeliryveryCheck = ({setLoginInfo}) => {
       setDriver(res1.data)
     // driver.map(() => {})
       console.log(res1.data)
+      console.log(res2.data)
       setOrder(res2.data)
+
     }))
     .catch((error) => {
       console.log(error)
     })
   },[])
   //선택
-  const [oneCheck, setOneCheck] = useState([]);
+  const statusChange = (newStatus = '배송중') => {
+    if (oneCheck.length === 0) {
+      alert('선택된 주문이 없습니다.');
+      return;
+    }
+    const selectedOrder = order.find((item) => oneCheck.includes(item.orderNum));
+  const dataToSend = {
+    deliveryNum : driver.deliveryNum,
+    driverName: driver.deliveryDriverName,
+    deliveryStatus: newStatus,
+    orderNum: selectedOrder.orderNum, // 선택된 주문 번호 리스트
+    deliveryAddress: selectedOrder.customerAddr // 배송 주소
+  };
+  console.log(dataToSend)
+  const url = (newStatus == '배송중') ? '/delivery/updateDriver' : '/delivery/endDriver'
+    axios.post(url, dataToSend)
+    .then((response) => {
+      console.log('배송 상태 업데이트 성공:', response.data);
+      // 상태가 성공적으로 변경되면 주문 목록을 다시 갱신
+      setOrder(response.data);
+      console.log(response.data)
+    })
+    .catch((error) => {
+      console.error('배송 상태 업데이트 실패:', error);
+    });
+  };
 
-  const statusChange = (id,newStatus) => {
-    const updatedDelivery = order.map((item, i) => {
-      return(item.deliveryNum == id ? {...item ,newStatus} : item
-    )})
-    setOneCheck(updatedDelivery)
-    
-  }
+  const [oneCheck, setOneCheck] = useState([]);
 
   //체크변경
   const changeCheck = (deliveryNum, checked) => {
-    setOneCheck((item) => 
-      checked 
-      ? [...item, deliveryNum] 
-      : item.filter((num) => num !== deliveryNum))
-    
-  }
+    setOneCheck((prevOneCheck) => 
+      checked ? [...prevOneCheck, deliveryNum] : prevOneCheck.filter((num) => num !== deliveryNum)
+    );
+  };
 
-  console.log(oneCheck)
   return (
     <div className='manage-contailner'>
       <div className='manage-main'>
@@ -110,31 +128,22 @@ const DeliryveryCheck = ({setLoginInfo}) => {
                   return(
                     <tr key={i}>
                       <td>
-                        <input type='checkbox' value={item.orderNum} onChange={(e) => setOneCheck(e.target.checked) }/>
+                        <input type='checkbox' value={item.orderNum} onChange={(e) => changeCheck(item.orderNum, e.target.checked) }/>
                       </td>
-                      <td>
-                        {item.orderNum}
-                      </td>
-                      <td>
-                        {item.orderNum}
-                      </td>
-                      <td>
-                        {item.orderNum}
-                      </td>
-                      <td>
-                        {item.orderNum}
-                      </td>
-                      <td>{}</td>
-                      <td>010-----</td>
-                      <td>
-                        배송출발
-                      </td>
-                      <td>배송중</td>
+                      <td>{i+1}</td>
+                      <td>{item.orderRequest.orderItemsVO.productName}</td>
+                      <td>{item.customerAddr}</td>
+                      <td>{item.totalPrice}</td>
+                      <td>{item.delivery.deliveryDriverName}</td>
+                      <td>{item.delivery.deliveryDriverPhone}</td>
+                      <td>{item.deliveryStatus}</td>
+                      <td>{item.orderStatus == '배송중' ? '배송중' : 
+                          item.orderStatus == '배송완료' ? '배송완료' : '배송 대기'}</td>
                     </tr>
                   )
                 })
               }
-                                    
+              
                 
             </tbody>
           </table>
