f10b501870c897a20d56733d4b3b3afdd8617ed5
hyoni91 <hyoni.green@gmail.com>
2024-10-18 17:43:41 +0900

    배송 목록

--- src/main/java/com/green/MediClick/provider/inventory/controller/InventoryController.java
+++ src/main/java/com/green/MediClick/provider/inventory/controller/InventoryController.java
@@ -1,6 +1,7 @@
 package com.green.MediClick.provider.inventory.controller;
 
 import com.green.MediClick.provider.inventory.service.InventoryService;
+import com.green.MediClick.provider.inventory.vo.InventoryVO;
 import com.green.MediClick.provider.item.vo.ItemListData;
 import com.green.MediClick.provider.item.vo.ItemVO;
 import com.green.MediClick.schedule.vo.PageVO;
@@ -14,5 +15,13 @@ import java.util.Map;
 @RestController
 @RequestMapping("/inventory")
 public class InventoryController {
+
+    @Resource(name = "inventoryService")
+    private InventoryService inventoryService;
+
+    @GetMapping("/list")
+    public List<InventoryVO> inventoryList(){
+        return inventoryService.inventoryList();
+    }
 
 }
